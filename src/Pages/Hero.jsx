c19795af0f51bde8c48e92805deadb89622a6405
Johan <johan.hark19@gmail.com>
2026-01-21 21:28:53 -0500

    style: actualizar imágenes a gifs y ajustar tamaño del perfil

--- src/Pages/Hero.jsx
+++ src/Pages/Hero.jsx
@@ -31,7 +31,7 @@ const Hero = () => {
                 </div>
 
                 <figure>
-                    <img src={cv.basics?.Imagen} alt="" className="aspect-square w-32 rounded-2xl " />
+                    <img src={cv.basics?.Imagen} alt="" className="object-contain w-40 rounded-2xl " />
                 </figure>
             </div>
 
